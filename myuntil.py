8201d9d2756ab7c11dafe783f61465726f4ff726
Aadesh <aadeshnpn@byu.edu>
2021-03-27 00:13:09 -0400

    Until tree done with random evaluation

--- myuntil.py
+++ myuntil.py
@@ -7,6 +7,7 @@ from py_trees.trees import BehaviourTree
 import py_trees
 from py_trees import common
 import copy
+import numpy as np
 
 
 class Delta1(Decorator):
@@ -152,6 +153,61 @@ class DeltaG(Decorator):
                 return common.Status.FAILURE
         return common.Status.SUCCESS
 
+class DeltaU(Decorator):
+    """
+    A decorator for the left sub-tree. 
+    """
+    def __init__(self, child, name=common.Name.AUTO_GENERATED):
+        """
+        Init with the decorated child.
+
+        Args:
+            child (:class:`~py_trees.behaviour.Behaviour`): behaviour to time
+            name (:obj:`str`): the decorator name
+        """
+        super(DeltaU, self).__init__(name=name, child=child)
+        self.psi1_false_yet = False
+        self.indx = 0
+        self.previous_value = common.Status.SUCCESS
+        self.previous_previous_value = common.Status.SUCCESS
+        self.always_failure = False
+
+    def update(self):
+        """
+        Flip :data:`~py_trees.common.Status.FAILURE` and
+        :data:`~py_trees.common.Status.SUCCESS`
+
+        Returns:
+            :class:`~py_trees.common.Status`: the behaviour's new status :class:`~py_trees.common.Status`
+        """
+        # if self.decorated.status == common.Status.SUCCESS:
+        #     self.start_append = True
+        if self.indx == 0:
+            self.beginning = False
+            return_value = common.Status.SUCCESS
+            self.previous_value = self.decorated.status
+        elif self.indx == 1:
+            if (self.previous_value == common.Status.SUCCESS and self.previous_previous_value == common.Status.SUCCESS):
+                return_value = common.Status.SUCCESS
+            else:
+                return_value = common.Status.FAILURE
+            self.previous_previous_value = self.previous_value                
+            self.previous_value = self.decorated.status
+        else:
+            if self.always_failure:
+                return_value = common.Status.FAILURE                
+            elif (self.previous_value == common.Status.FAILURE or self.previous_previous_value == common.Status.FAILURE):
+                self.always_failure = True
+                return_value = common.Status.FAILURE                                
+            elif (self.previous_value == common.Status.SUCCESS and self.previous_previous_value == common.Status.SUCCESS):
+                return_value = common.Status.SUCCESS
+            self.previous_previous_value = self.previous_value
+            self.previous_value = self.decorated.status
+        # print('previous value',self.indx, self.previous_previous_value, self.previous_value, self.decorated.status)            
+        self.indx +=1        
+        return return_value
+
+
 
 class LTLNode(py_trees.behaviour.Behaviour):
     """LTL node for the proving decomposition.
@@ -197,6 +253,61 @@ def setup_nodes(nodes, i, trace):
     nodes[3].setup(0, 'b', trace[i])        
 
 
+def setup_nodesu(nodes, i, trace):
+    # print('a,b', i, trace[i])
+    nodes[0].setup(0, 'a', trace[i])    
+    nodes[1].setup(0, 'b', trace[i])        
+
+
+
+def neweruntil(trace):
+    # Left sub-tree
+    seleleft = Sequence('Left')
+    goal1 = LTLNode('g1')
+    goal2 = LTLNode('g2')    
+    deltau = DeltaU(goal1)
+    seleleft.add_children([deltau, goal2])
+    
+    top = Delta3(seleleft)
+    root = BehaviourTree(top)
+    i = 0
+
+    # Creating a LTLf parser object
+    parser = LTLfParser()
+    # Until goal specification
+    formula = "(a U b)"
+    # Parsed formula
+    parsed_formula = parser(formula)
+
+    # py_trees.logging.level = py_trees.logging.Level.DEBUG
+    output = py_trees.display.ascii_tree(root.root)
+    # print(output)
+    
+    ltl_status = parsed_formula.truth(trace)
+
+    for k in range(len(trace)):
+        setup_nodesu([goal1, goal2], i, trace)
+        root.tick()
+        # print(i, common.Status.SUCCESS)
+        i += 1
+
+    bt_status = root.root.status
+    
+    print(ltl_status, bt_status)
+
+    if (bt_status == common.Status.SUCCESS and ltl_status is True):
+        return True
+    elif (bt_status == common.Status.FAILURE and ltl_status is False):
+        return True
+    else:
+        return False
+    # if root.root.status == common.Status.SUCCESS:
+    #     return True
+    # else:
+    #     return False
+    # print(root.root.status)
+
+
 def newuntil(trace):
     main = Sequence('R')
 
@@ -240,7 +351,7 @@ def newuntil(trace):
         setup_nodes([goal1, goal11, goal2, goal22], i, trace)
         root.tick()
         # print(i, common.Status.SUCCESS)
-        print(i, parsed_formula.truth(trace, k), root.root.status)        
+        print(i, parsed_formula.truth(trace), root.root.status)        
         i += 1
 
     # if root.root.status == common.Status.SUCCESS:
@@ -310,20 +421,22 @@ def main(fn):
 
     # First just testing four canonical boolean variables
     trace = [t1, t2, t3, t4]
-
+    count = 0
     for t in trace:
         print('Trace', t)
         # Call the skeleton function that implements a minimal BT
         # for Until-sub tree based on the Figure from Dr.Goodrich
         # paper draft
-        fn(t)
+        return1 = fn(t)
+        if not return1:
+            count += 1
         print('------')
 
     t5 = [ 
          [t1[0], t1[0]],
          [t1[0], t2[0]],   
          [t1[0], t3[0]],        
-         [t1[0], t4[0]],
+        [t1[0], t4[0]],
          [t2[0], t1[0]],
          [t2[0], t2[0]],   
          [t2[0], t3[0]],        
@@ -339,12 +452,183 @@ def main(fn):
          ]
     for t in t5:
         print('Trace', t)
-        fn(t)
+        return1 = fn(t)
+        if not return1:
+            count += 1
         print('------')         
+     
+    
+    print("total trace: {}, Traces not agreed: {}".format(len(t5)+ len(trace), count))
+
+    # t6 = [{
+    #     'a': False, 'b': False},
+    #     {'a': True, 'b': True,}
+    #     ]
+    # fn(t6)
+
+def main1(fn):
+    count = 0
+    t1 = [{
+        'a': False, 'b': False
+    }]
+    t2 = [{
+        'a': False, 'b': True
+    }]
+    t3 = [{
+        'a': True, 'b': False
+    }]
+    t4 = [{
+        'a': True, 'b': True
+    }]    
+    t6 = [ 
+         [t1[0], t1[0], t1[0]],
+         [t1[0], t1[0], t2[0]],   
+         [t1[0], t1[0], t3[0]],        
+         [t1[0], t1[0], t4[0]],
+
+         [t1[0], t2[0], t1[0]],
+         [t1[0], t2[0], t2[0]],   
+         [t1[0], t2[0], t3[0]],        
+         [t1[0], t2[0], t4[0]],         
+
+
+         [t1[0], t3[0], t1[0]],
+         [t1[0], t3[0], t2[0]],   
+         [t1[0], t3[0], t3[0]],        
+         [t1[0], t3[0], t4[0]],         
+
+
+         [t1[0], t4[0], t1[0]],
+         [t1[0], t4[0], t2[0]],   
+         [t1[0], t4[0], t3[0]],        
+         [t1[0], t4[0], t4[0]]
+    ]
+
+    t7 = [ 
+         [t2[0], t2[0], t1[0]],
+         [t2[0], t2[0], t2[0]],   
+         [t2[0], t2[0], t3[0]],        
+         [t2[0], t2[0], t4[0]],
+
+         [t2[0], t3[0], t1[0]],
+         [t2[0], t3[0], t2[0]],   
+         [t2[0], t3[0], t3[0]],        
+         [t2[0], t3[0], t4[0]],         
+
+
+         [t2[0], t4[0], t1[0]],
+         [t2[0], t4[0], t2[0]],   
+         [t2[0], t4[0], t3[0]],        
+         [t2[0], t4[0], t4[0]],         
+
+
+         [t2[0], t1[0], t1[0]],
+         [t2[0], t1[0], t2[0]],   
+         [t2[0], t1[0], t3[0]],        
+         [t2[0], t1[0], t4[0]]
+    ]    
+
+    t8 = [ 
+         [t3[0], t3[0], t1[0]],
+         [t3[0], t3[0], t2[0]],   
+         [t3[0], t3[0], t3[0]],        
+         [t3[0], t3[0], t4[0]],
+
+         [t3[0], t4[0], t1[0]],
+         [t3[0], t4[0], t2[0]],   
+         [t3[0], t4[0], t3[0]],        
+         [t3[0], t4[0], t4[0]],         
+
+
+         [t3[0], t1[0], t1[0]],
+         [t3[0], t1[0], t2[0]],   
+         [t3[0], t1[0], t3[0]],        
+         [t3[0], t1[0], t4[0]],         
+
+
+         [t3[0], t2[0], t1[0]],
+         [t3[0], t2[0], t2[0]],   
+         [t3[0], t2[0], t3[0]],        
+         [t3[0], t2[0], t4[0]]
+    ]     
+
+    t9 = [ 
+         [t4[0], t4[0], t1[0]],
+         [t4[0], t4[0], t2[0]],   
+         [t4[0], t4[0], t3[0]],        
+         [t4[0], t4[0], t4[0]],
+
+         [t4[0], t1[0], t1[0]],
+         [t4[0], t1[0], t2[0]],   
+         [t4[0], t1[0], t3[0]],        
+         [t4[0], t1[0], t4[0]],         
+
+
+         [t4[0], t2[0], t1[0]],
+         [t4[0], t2[0], t2[0]],   
+         [t4[0], t2[0], t3[0]],        
+         [t4[0], t2[0], t4[0]],         
+
+
+         [t4[0], t3[0], t1[0]],
+         [t4[0], t3[0], t2[0]],   
+         [t4[0], t3[0], t3[0]],        
+         [t4[0], t3[0], t4[0]]
+    ]            
+
+    for t in t6 + t7 + t8 + t9:
+        print('Trace', t)
+        return1 = fn(t)
+        if not return1:
+            count += 1
+        print('------')  
+    print("total trace: {}, Traces not agreed: {}".format(len(t6) + len(t7) + len(t8) + len(t9), count))
+
+
+def mainrand(fn):
+    count = 0
+    n = 100
+    t1 = [{
+        'a': False, 'b': False
+    }]
+    t2 = [{
+        'a': False, 'b': True
+    }]
+    t3 = [{
+        'a': True, 'b': False
+    }]
+    t4 = [{
+        'a': True, 'b': True
+    }]  
+    tracelist = []
+    def gettraces():
+        m = np.random.randint(1, 50)
+        choices = {
+            0: t1[0],
+            1: t2[0],
+            2: t3[0],
+            3: t4[0]
+        }
+        trace = []
+        for i in range(m):
+            j = np.random.choice([0,1,2,3], m)
+            trace.append(choices[j[i]])
+        return trace
+    
+    for k in range(n):        
+        return1 = fn(gettraces())
+        if not return1:
+            count += 1 
+    
+    print("total trace: {}, Traces not agreed: {}".format(n, count))
 
 
 if __name__ == '__main__':
     # For old unitl 
     # main(olduntil)
     # For new until    
-    main(newuntil)
+    # main(newuntil)
+    # For newer until
+    # main(neweruntil)    
+    # main1(neweruntil)        
+    mainrand(neweruntil)
