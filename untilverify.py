b73966ffcc231291d73e865b4263a692fa8faf79
Aadesh <aadeshnpn@byu.edu>
2021-05-10 12:50:03 -0400

    teting a goal specificint for swarm goals

--- untilverify.py
+++ untilverify.py
@@ -1,23 +1,44 @@
 from flloat.parser.ltlf import LTLfParser
 
- 
+
+
+def testing():
+    parser = LTLfParser()
+
+    # formula_string = "((X a) & (b)) U c"
+    formula_string = " (G (F a))"
+
+    formula = parser(formula_string)        # returns a LTLfFormula
+
+    t1 = [
+        {'a': False, 'b': True, 'c': False},
+        {'a': False, 'b': False, 'c': False},
+        {'a': False, 'b': True, 'c': True},
+        {'a': True, 'b': True, 'c': False},
+        {'a': True, 'b': False, 'c': False},
+        {'a': False, 'b': True, 'c': True}
+        ]
+
+    print(formula.truth(t1))
+
 
 def main():
 
     parser = LTLfParser()
 
     # formula_string = "((X a) & (b)) U c"
-    formula_string = " (X (a & b))"    
+    formula_string = " (X (a & b))"
 
     formula = parser(formula_string)        # returns a LTLfFormula
 
     t1 = [
-        {'a': True, 'b': True, 'c': False}, 
+        {'a': True, 'b': True, 'c': False},
         {'a': True, 'b': False, 'c': False},
         {'a': False, 'b': True, 'c': True}
         ]
-    
+
     print(formula.truth(t1))
 
-main()    
-   
+# main()
+
+testing()
