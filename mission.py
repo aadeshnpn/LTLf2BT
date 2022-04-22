9e6958e581524ef62660ca07b57d669ea43c0af6
Aadesh <aadeshnpn@byu.edu>
2022-04-21 21:37:05 -0400

    Mission until working

--- mission.py
+++ mission.py
@@ -182,12 +182,76 @@ def mission_sequential():
     print('Task (T, F)', formula1.truth(trace3))
 
 
+def mission_until():
+    # Task 1 fail and task 2 success
+    trace1 = [
+        {'p': True, 'g': True, 't': True, 'o': False,'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': True, 'g': True, 't': True, 'o': False,'q': True, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': True},
+    ]
+
+    # Task 1 succeed and Task 2 succeed
+    trace2 = [
+        {'p': True, 'g': True, 't': True, 'o': False,'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': True, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': True, 'g': True, 't': True, 'o': False,'q': True, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': True},
+    ]
+
+    # Task 1 succed and Task 2 fail
+    trace3 = [
+        {'p': True, 'g': True, 't': True, 'o': False,'q': True, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': True, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False,'q': True, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+    ]
+
+    trace4 = [
+        {'p': True, 'g': True, 't': True, 'o': False,'q': True, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False,'q': True, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+    ]
+
+    psi1 = "((G (g) & o) |( (G (g) & (p)) & ( (G (g) & t) U ((G (g) & o)))))"
+    psi2 = "((G (h) & n) |( (G (h) & (F(q))) & ( (G (h) & u) U ((G (h) & n)))))"
+    psi = psi1 + 'U ' + psi2
+    parser = LTLfParser()
+    fpsi = parser(psi)
+    fpsi1 = parser(psi1)
+    fpsi2 = parser(psi2)
+    print('Task (F, T)', fpsi.truth(trace1), fpsi1.truth(trace1), fpsi2.truth(trace1))
+    print('Task (T, T)', fpsi.truth(trace2), fpsi1.truth(trace2), fpsi2.truth(trace2))
+    print('Task (T, F)', fpsi.truth(trace3), fpsi1.truth(trace3), fpsi2.truth(trace3))
+    print('Task (F, F)', fpsi.truth(trace4), fpsi1.truth(trace4), fpsi2.truth(trace4))
+    # print('Task (T, T)', formula1.truth(trace2))
+    # print('Task (T, F)', formula1.truth(trace3))
+
+
 def main():
     # mission_only_task()
     # mission_finally()
     # mission_or()
     # mission_and()
-    mission_sequential()
+    # mission_sequential()
+    mission_until()
 
 
 if __name__ == '__main__':
