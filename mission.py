3222dd327a57d1b482e0cfc291201785e233e5c6
Aadesh <aadeshnpn@byu.edu>
2022-04-21 20:44:17 -0400

    mission And operator only works when finally operator is introduced for pre-condition

--- mission.py
+++ mission.py
@@ -78,10 +78,72 @@ def mission_or():
     print('Task 1 fail and Task 2 failed', formula1.truth(trace3))
 
 
+def mission_and():
+    # p,g,t,o - > q,h,u,n
+    # Task 1 failed and Task 2 succeed
+    # Finally operator for precondition needed for and to work
+    trace1 = [
+        {'p': True, 'g': True, 't': True, 'o': False,'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False,'q': True, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': True},
+    ]
+    # Task 1 succeed and Task 2 succeed
+    trace2 = [
+        {'p': True, 'g': True, 't': True, 'o': False,'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': True, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': True, 'g': True, 't': True, 'o': False,'q': True, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': True},
+    ]
+    # Task 1 succed and Task 2 fail
+    trace3 = [
+        {'p': True, 'g': True, 't': True, 'o': False,'q': True, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': True, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False,'q': True, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+    ]
+
+    # Task 1 fail and Task 2 fail
+    trace4 = [
+        {'p': True, 'g': True, 't': True, 'o': False,'q': True, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False,'q': True, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+        {'p': False, 'g': True, 't': True, 'o': False, 'q': False, 'h': True, 'u': True, 'n': False},
+    ]
+    psi1 = "((G (g) & o) |( (G (g) & (p)) & ( (G (g) & t) U ((G (g) & o)))))"
+    psi2 = "((G (h) & n) |( (G (h) & (F(q))) & ( (G (h) & u) U ((G (h) & n)))))"
+    psi = psi1 + '& ' + psi2
+    parser = LTLfParser()
+    formula1 = parser(psi)
+    print('Task (F, T)', formula1.truth(trace1))
+    print('Task (T, T)', formula1.truth(trace2))
+    print('Task (T, F)', formula1.truth(trace3))
+    print('Task (F, F)', formula1.truth(trace4))
+    # print('Task 1 fail and Task 2 success', formula1.truth(trace2))
+    # print('Task 1 fail and Task 2 failed', formula1.truth(trace3))
+
+
 def main():
     # mission_only_task()
     # mission_finally()
-    mission_or()
+    # mission_or()
+    mission_and()
 
 
 if __name__ == '__main__':
